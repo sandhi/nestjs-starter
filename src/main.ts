2f2a55188ec1c0553dcec7327d1df8a0471b5b96
Sandhi Zulqifli <sandhi.yz@gmail.com>
2021-08-16 22:27:57 +0700

    added csrf security

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import fastifyCookie from 'fastify-cookie';
+import fastifyCsrf from 'fastify-csrf';
 import { fastifyHelmet } from 'fastify-helmet';
 import { AppModule } from './app.module';
 
@@ -14,6 +16,8 @@ async function bootstrap() {
   );
 
   await app.register(fastifyHelmet);
+  await app.register(fastifyCookie);
+  await app.register(fastifyCsrf);
 
   app.useGlobalPipes(
     new ValidationPipe({
